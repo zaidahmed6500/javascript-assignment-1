5a076e62d2a6dd5b2a31ef09aabb61cdbed822e0
zaidahmed6500 <zaidahmed6500@gmail.com>
2025-05-31 05:05:25 +0500

    changes

---
+++ app.js
@@ -0,0 +1,26 @@
+var name = prompt("What is your name?");
+alert("Welcome, " + name + "!");
+
+
+
+var name = prompt("What is your name?");
+var age = prompt("How old are you?");
+console.log(name + " is " + age + " years old.");
+
+
+
+var color = prompt("What is your favorite color?");
+alert("Wow! " + color + " is a nice color!");
+
+
+var num1 = prompt("Enter the first number:");
+var num2 = prompt("Enter the second number:");
+var result = num1 + num2;  // Strings will be concatenated, not added numerically
+console.log("The result of " + num1 + " + " + num2 + " is " + result + ".");
+
+
+
+var name = prompt("What is your name?");
+var hobby = prompt("What is your hobby?");
+var country = prompt("Which country are you from?");
+alert("Hi, I'm " + name + " from " + country + ", and I love " + hobby + "!");
